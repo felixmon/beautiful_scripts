3be014b760bcc7c7c58fafbf3f14e5f2babd94cc
felixmon <felix.mon.cn@gmail.com>
2020-04-01 22:11:50 +0800

    add split with awk

--- beautiful_shell_scripts.sh
+++ beautiful_shell_scripts.sh
@@ -1,3 +1,4 @@
+# find and show filenames only
 find . -type f  -exec basename {} \;
 <<COMMENT
 https://qr.ae/pNvZwW
@@ -5,3 +6,6 @@ https://qr.ae/pNvZwW
 The whole command is generally equivalent to
 for f in `find .`; do basename ${f}; done
 COMMENT
+
+# split with awk
+echo "12|23|11" | awk '{split($0,a,"|"); print a[3],a[2],a[1]}'
