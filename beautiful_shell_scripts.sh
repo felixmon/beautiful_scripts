cf0b4d44e8159722fdc6fc816f403ce9bc6575e7
felixmon <felix.mon.cn@gmail.com>
2020-04-01 22:23:59 +0800

    update Wed Apr  1 22:23:59 CST 2020

--- beautiful_shell_scripts.sh
+++ beautiful_shell_scripts.sh
@@ -7,5 +7,8 @@ The whole command is generally equivalent to
 for f in `find .`; do basename ${f}; done
 COMMENT
 
-# split with awk
+# split with awk https://stackoverflow.com/a/8009724/13130970
 echo "12|23|11" | awk '{split($0,a,"|"); print a[3],a[2],a[1]}'
+# or https://stackoverflow.com/a/8009855/13130970
+echo "12|23|11|bb|dd|aaa|badfasjlkdjalsg|g" | awk -F\| '{for (i = 0; ++i <= NF;)print i, $i}'
+echo "/Users/felix/Documents/LEARN/Science/Computer_Science/Programming/Shell_Programming/Shell_Scripts" | awk -F\/ '{for (i = 0; ++i <= NF;)print i, $i}'
