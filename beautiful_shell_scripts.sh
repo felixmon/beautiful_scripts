be070dc2039c96de90b2d13ffff9368a384f7904
felixmon <felix.mon.cn@gmail.com>
2020-04-01 16:36:12 +0800

    update

--- beautiful_shell_scripts.sh
+++ beautiful_shell_scripts.sh
@@ -1,7 +1,7 @@
 find . -type f  -exec basename {} \;
-/*
+<<COMMENT
 https://qr.ae/pNvZwW
 {} is the placeholder to pass a single file name to -exec called command, and \; is to finish -exec.
 The whole command is generally equivalent to
 for f in `find .`; do basename ${f}; done
-*/
+COMMENT
